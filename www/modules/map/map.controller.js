dfded66dce6af488476acc8ce3a1613390995bc1
apivetta <aguspivetta@gmail.com>
2016-05-31 14:41:57 +0000

    boton location en mapa

--- www/modules/map/map.controller.js
+++ www/modules/map/map.controller.js
@@ -1,84 +1,77 @@
 'use strict';
 angular.module('modules')
-.controller('MapController', ['$log', '$state','$cordovaDevice', '$cordovaGeolocation','locationService','paradasService',MapController]);
+  .controller('MapController', ['$log', '$state', '$cordovaDevice', 'locationService', 'paradasService', MapController]);
 
 
-function MapController($log,$state, $cordovaDevice,$cordovaGeolocation,locationService,paradasService) {
-		var vm = this;
+function MapController($log, $state, $cordovaDevice, locationService, paradasService) {
+  var vm = this;
 
-    vm.positionMarker = {} ;
-    vm.map = { } ; 
-      createMap();
-      vm.openWizard = openWizard; 
-      vm.locate = locate;
+  vm.positionMarker = {};
+  vm.map = {};
+  createMap();
+  vm.openWizard = openWizard;
+  vm.locate = locate;
 
-     	function openWizard (){
-  			$state.go('app.wizard.step1');
+  function openWizard() {
+    $state.go('app.wizard.step1');
+  }
 
+  function createMap() {
+    locationService.locate().then(onSuccess, onFail);
 
-     	}
+    function onSuccess(data) {
+      vm.map = L.map('map').setView([data.lat, data.lng], 16);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(vm.map);
 
+      vm.positionMarker = L.marker([data.lat, data.lng], { draggable: true }).addTo(vm.map)
+        .bindPopup('Posicion actual \nprecision: ' + data.accuracy + " mts")
+        .openPopup();
 
-      function createMap(){
-        locationService.locate().then(onSuccess,onFail);
+      mapParadas();
+    };
 
-        function onSuccess (data){
-          vm.map = L.map('map').setView([data.lat,data.lng],16);
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(vm.map);
+    function onFail(error) {
 
-            vm.positionMarker = L.marker([data.lat,data.lng],{draggable:true}).addTo(vm.map)
-                .bindPopup('Posicion actual \nprecision: '+data.accuracy+" mts")
-                .openPopup();
-
-            mapParadas();
-        };
-
-        function onFail    (error){
-
-        };
+    };
 
 
-        function mapParadas(){
-          paradasService.list().then(function (data){
+    function mapParadas() {
+      paradasService.list().then(function(data) {
 
-            console.log(data);
-            data.forEach(function (element,index){
-              
-              var coords = [element.lat,element.lng];
+        console.log(data);
+        data.forEach(function(element, index) {
+
+          var coords = [element.lat, element.lng];
 
 
-            //L.circle(coords,5).addTo(vm.map)
+          //L.circle(coords,5).addTo(vm.map)
 
-            L.marker(coords,{clickable:true,draggable:false})
-              .addTo(vm.map)
-              .on('click',function(e){
-                console.log(e);
-                console.log(element.properties);
-                $state.go('app.detail',{paradaID:element.id})
-              });
+          L.marker(coords, { clickable: true, draggable: false })
+            .addTo(vm.map)
+            .on('click', function(e) {
+              console.log(e);
+              console.log(element.properties);
+              $state.go('app.detail', { paradaID: element.id })
             });
+        });
 
+      });
+    }
 
+  }
 
-          });
-        }
-
-      }
-
-     	function locate(){
-
-        // navigator.geolocation.getCurrentPosition(function(data){
-        //   console.log(data);
-        // });
-
-
-
-
-      };
+  function locate() {
+    locationService.locate().then(function(data) {
+      console.log(data);
+      vm.map.panTo(new L.LatLng(data.lat, data.lng), {
+        animate: true,
+        duration: 0.5
+      });
+    });
+  };
 
   $log.log('Hello from your Controller: MapController in module main:. This is your controller:', this);
 
 }
-      
